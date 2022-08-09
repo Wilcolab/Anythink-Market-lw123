226e54655a210abd5593775d252967304fbe1e0d
oliver <oliver.conzelmann@gmx.de>
2022-08-09 10:01:10 +0200

    Added UI Search TextInput

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -9,6 +9,7 @@ import {
   HOME_PAGE_UNLOADED,
   APPLY_TAG_FILTER,
 } from "../../constants/actionTypes";
+import ItemList from "../ItemList";
 
 const Promise = global.Promise;
 
@@ -36,7 +37,39 @@ class Home extends React.Component {
       itemsPromise,
       Promise.all([agent.Tags.getAll(), itemsPromise()])
     );
+
+    this.setState({searchResults: []});
   }
+
+  changeSearchInput = (e) =>{
+   
+
+    console.log(  String(e.target.value).length );
+
+    if(String(e.target.value).length >= 3) {
+
+      console.log('Call API');
+
+      fetch('http://localhost:3000/api/items?title='+String(e.target.value))
+        .then((response) => response.json() )
+        .then( (data) => {
+          console.log(data.items);
+
+          const items = data.items;
+
+          this.setState({searchResults: items});
+
+        } )
+
+    }
+
+     
+
+    
+
+
+  }
+
 
   componentWillUnmount() {
     this.props.onUnload();
@@ -46,6 +79,27 @@ class Home extends React.Component {
     return (
       <div className="home-page">
         <Banner />
+
+        <div className="container p-4 text-center">
+          <input type="text" id="search-box" onChange={this.changeSearchInput} ></input>
+
+            <br />
+
+            {/* {this.state.searchResults.map((item, index) => { 
+
+              const line = item.title + " " + item.description;
+
+              return line;
+
+              })} */}
+
+              <ItemList 
+                items={this.state.searchResults}
+              ></ItemList>
+
+        </div>
+
+        
 
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
